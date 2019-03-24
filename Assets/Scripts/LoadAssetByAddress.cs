7e2f498ce14f5d6ccf0d1f7a331322eb5d6cc265
prasetion <prasetio.nugroho@octagonstudio.com>
2019-03-24 16:35:10 +0700

    add script

---
+++ Assets/Scripts/LoadAssetByAddress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LoadAssetByAddress : MonoBehaviour
+{
+    [SerializeField] private string _AssetAddress;
+    [SerializeField] private GameObject _MyGameObject;
+
+    // Start is called before the first frame update
+    void Start(){
+        LoadedAsset(_AssetAddress);
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update(){
+        if(Input.GetKeyUp(KeyCode.Space)){
+            Instantiate(_MyGameObject);
+        }
+    }
+
+    void LoadedAsset(string addressName){
+        UnityEngine.AddressableAssets.Addressables.LoadAsset<GameObject>(addressName).Completed += LoadDone;
+    }
+
+    void LoadDone(UnityEngine.ResourceManagement.AsyncOperations.IAsyncOperation<GameObject> obj){
+        _MyGameObject = obj.Result;
+        Debug.Log("finish load asset");
+    }
+
+}
---
+++ Assets/Scripts/LoadAssetByObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetByObject : MonoBehaviour{
+
+    public UnityEngine.AddressableAssets.AssetReference Barrel;
+    [SerializeField] private GameObject _MyGameObject;
+
+    void Start(){
+       LoadedAsset();
+    }
+
+    void Update(){
+        if(Input.GetKeyUp(KeyCode.Space)){
+            Instantiate(_MyGameObject);
+        }
+    }
+
+    void LoadedAsset(){
+        UnityEngine.AddressableAssets.LoadAsset<GameObject>().Completed += LoadDone;
+    }
+
+    void LoadDone(UnityEngine.ResourceManagement.AsyncOperations.IAsyncOperation<GameObject> obj){
+        _MyGameObject = obj.Result;
+        Debug.Log("finish load asset");
+    }
+
+}
